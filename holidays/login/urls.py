42b5237526f596614b2183712ba392fc64ce1572
Matt Leach <matt.leach25@gmail.com>
2014-12-28 23:56:09 +0000

    Adding login app with intial home view

--- holidays/login/urls.py
+++ holidays/login/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from views import HomeView
 
 urlpatterns = patterns('',
-    url(r'^$', include('login.urls')),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', HomeView.as_view(), name='home'),
 )
