45a3b172462ff3f0bd5affd66be1225e28960877
Matt Leach <matt.leach25@gmail.com>
2014-12-28 18:37:23 +0000

    Moving settings etc into base directory
    To avoid potential name issue when I create holidays app

--- holidays/settings.py
+++ holidays/settings.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+BASE_DIR = os.path.dirname(__file__)
 
 
 # Quick-start development settings - unsuitable for production
@@ -48,9 +48,9 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-ROOT_URLCONF = 'holidays.urls'
+ROOT_URLCONF = 'urls'
 
-WSGI_APPLICATION = 'holidays.wsgi.application'
+WSGI_APPLICATION = 'wsgi.application'
 
 
 # Database
