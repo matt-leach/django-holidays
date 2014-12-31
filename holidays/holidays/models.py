0ad0f5a7a8ff9e6b990c3966e0f5b0534cccc6d4
Matt <matt.leach25@gmail.com>
2014-12-31 17:45:19 +0000

    Adding approval to Holiday model

--- holidays/holidays/models.py
+++ holidays/holidays/models.py
@@ -6,7 +6,8 @@ class Holiday(models.Model):
     user = models.ForeignKey(User, related_name="holidays")
     start_date = models.DateField()
     end_date = models.DateField()
-    
+    approved = models.NullBooleanField(default=None)
+    approved_by = models.ForeignKey(User, null=True, blank=True)
     
     def __unicode__(self):
         return "Holiday for %s: %s - %s" % (self.user, self.start_date, self.end_date)
