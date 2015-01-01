92388fec956eddb71ece9e1d72007fe4906d8a61
Matt <matt.leach25@gmail.com>
2015-01-01 11:34:29 +0000

    Adding ajax request to pull in holidays from previous years

--- holidays/holidays/urls.py
+++ holidays/holidays/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from holidays.views import AddHolidayView
+
+from holidays.views import AddHolidayView, GetYearlyHolidaysView
 
 urlpatterns = patterns('',
     url(r'^add/$', AddHolidayView.as_view(), name='user'),
+    url(r'^getYearly/$', GetYearlyHolidaysView.as_view(), name='get_yearly'),
 )
--- holidays/holidays/views.py
+++ holidays/holidays/views.py
@@ -1,5 +1,23 @@
 from django.views import generic
+from django.contrib.auth.models import User
+
 from holidays.models import Holiday
+
+
 
 class AddHolidayView(generic.CreateView):
     model = Holiday
+    
+    
+class GetYearlyHolidaysView(generic.TemplateView):
+    template_name = "holidays/holidayTable.html"
+    
+    def get_context_data(self, **kwargs):
+        context = super(GetYearlyHolidaysView, self).get_context_data(**kwargs)
+        
+        # This can easily fail
+        user_id = self.request.GET["userid"]
+        context['staff'] = User.objects.get(id=user_id)
+        context['year'] = self.request.GET["year"]
+
+        return context
